0fd4fba2ca595e5a69d03aee6af8174ead890e97
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-28 16:08:05 +0100

    done

--- Solution.java
+++ Solution.java
@@ -1,22 +1,17 @@
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
 
 
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = nums.length-1; i>= 0; i--) {
-            for (int j = 0; j < nums.length; j++) {
-                    if (nums[i]+ nums[j] == target && i != j){
-                        result[0] = i;
-                        result[1] = j;
-                        break;
-                    }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
-
-        return result;
+        throw new IllegalArgumentException("No two sum solution");
     }
-
 }
--- main.java
+++ main.java
@@ -4,6 +4,8 @@ public class main {
     public static void main(String[] args) {
         //[3,2,4]
         //[3,3]
+        //[0,4,3,0]
+        //[2,4,11,3]
         int [] test = new int[4];
 //        test[0] = 3;
 //        test[1] = 2;
@@ -11,10 +13,10 @@ public class main {
 //          test[0] = 3;
 //          test[1] = 3;
         test[0] = 2;
-        test[1] = 5;
-        test[2] = 5;
-        test[3] = 11;
-        int target = 10;
+        test[1] = 4;
+        test[2] = 11;
+        test[3] = 3;
+        int target = 6;
 
         Solution solution = new Solution();
 
